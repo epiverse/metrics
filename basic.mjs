1b7f0f9834c8aee90e5b3ebc08632bd371acca2f
Jonas Almeida <jalmeida@mathbiol.org>
2025-11-04 11:20:01 -0500

    cosine

--- basic.mjs
+++ basic.mjs
@@ -4,5 +4,22 @@ function euclid(a=[1,2,3],b=[4,5,6]){ // default
     // Euclidean distance
     return Math.sqrt(a.map((ai,i)=>(ai-b[i])**2).reduce((x,y)=>x+y))
 }
+// Dev Notes
+// ((await import('https://epiverse.github.io/metrics/basic.mjs')).euclid)([1,2,3],[4,5,6])
+// ((await import('http://localhost:8000/metrics/basic.mjs')).euclid)([1,2,3],[4,5,6])
 
-export {euclid}
+function SSSV(x){ // squared root of sum of squared values
+    return Math.sqrt(x.map(xi=>xi**2).reduce((a,b)=>(a+b)))
+}
+
+function cosine(a=[1,2,3],b=[4,5,6]){
+    // Cosine similarity = 1 - cosine distance
+    // AB/(A.B) 
+    let AB = a.map((ai,i)=>ai*b[i])
+    AB = AB.reduce((v1,v2)=>(v1+v2))
+    let A = SSSV(a)
+    let B = SSSV(b)
+    return AB/(A*B)
+}
+
+export {euclid,cosine}
