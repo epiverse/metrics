ebc61e3acf28030983ea741db3c76c3e9ab297e8
Jonas Almeida <jalmeida@mathbiol.org>
2025-11-10 11:58:21 -0500

    corr
    Pearson correlation coefficient

--- basic.mjs
+++ basic.mjs
@@ -8,7 +8,7 @@ function euclid(a=[1,2,3],b=[4,5,6]){ // default
 // ((await import('https://epiverse.github.io/metrics/basic.mjs')).euclid)([1,2,3],[4,5,6])
 // ((await import('http://localhost:8000/metrics/basic.mjs')).euclid)([1,2,3],[4,5,6])
 
-function SSSV(x){ // squared root of sum of squared values
+function SSV(x){ // squared root of sum of squared values
     return Math.sqrt(x.map(xi=>xi**2).reduce((a,b)=>(a+b)))
 }
 
@@ -17,8 +17,8 @@ function cosine(a=[1,2,3],b=[4,5,6]){
     // AB/(A.B) 
     let AB = a.map((ai,i)=>ai*b[i])
     AB = AB.reduce((v1,v2)=>(v1+v2))
-    let A = SSSV(a)
-    let B = SSSV(b)
+    let A = SSV(a)
+    let B = SSV(b)
     return 1-AB/(A*B) // distance = 1-similarity
 }
 
@@ -27,7 +27,11 @@ function corr(x=[1,2,3],y=[4,5,6]){
     let n = x.length
     let avg_x = x.reduce((a,b)=>a+b)/n
     let avg_y = y.reduce((a,b)=>a+b)/n
-    return 'under development'
+    let dx = x.map(xi=>(xi-avg_x))
+    let dy = y.map(yi=>(yi-avg_y))
+    let dd = dx.map((dxi,i)=>dxi*dy[i])
+    dd = dd.reduce((a,b)=>a+b)
+    return dd/(SSV(dx)*SSV(dy))
 }
 
 export {euclid,cosine,corr}
