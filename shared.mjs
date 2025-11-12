9b8ff8daad0d77e1dedc019e6c5319032747aaae
Jonas Almeida <jalmeida@mathbiol.org>
2025-11-12 10:24:50 -0500

    unique nn started

--- shared.mjs
+++ shared.mjs
@@ -1,1 +1,8 @@
-// metrics based on shared neighbors
+// metrics based on unique shared neighbors
+
+function unique(a=[1,2,3],b=[4,5,6]){
+    // unique share neighbors
+    return 'under development' // distance = 1-similarity
+}
+
+export{unique}
